ad078aa48707c0620d9acbfcbb9ea1b13e1bfbe1
Zulisa <zrodri2338@my.sullivan.edu>
2025-11-09 16:54:48 -0500

    Fixed some code

--- index/fullstack-curd/common.php
+++ index/fullstack-curd/common.php
@@ -1,5 +1,11 @@
 <?php
+session_start();
+
+if (empty($_SESSION['csrf'])) {
+    $_SESSION['csrf'] = bin2hex(random_bytes(32));
+}
+
 function escape($html) {
-  return htmlspecialchars($html, ENT_QUOTES | ENT_SUBSTITUTE, "UTF-8");
+    return htmlspecialchars($html, ENT_QUOTES | ENT_SUBSTITUTE, "UTF-8");
 }
 ?>
--- index/fullstack-curd/public/templates/footer.php
+++ index/fullstack-curd/public/templates/footer.php
@@ -1,2 +1,3 @@
+<HR>
 </body>
 </html>
